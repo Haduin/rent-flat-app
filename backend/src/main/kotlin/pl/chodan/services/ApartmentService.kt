cd6bc19c2d9a3a3a407ac3869d5c10cdf302a770
Chodań <jakub.chodan@softiq.pl>
2025-06-24 07:47:40 +0200

    fixy

--- backend/src/main/kotlin/pl/chodan/services/ApartmentService.kt
+++ backend/src/main/kotlin/pl/chodan/services/ApartmentService.kt
@@ -1,13 +1,10 @@
 package pl.chodan.services
 
-import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
 import pl.chodan.Apartment
 import pl.chodan.ApartmentAndRoomNumberDTO
+import pl.chodan.Room
 import pl.chodan.dbQuery
 
 class ApartmentService {
@@ -22,26 +19,17 @@ class ApartmentService {
     }
 
     suspend fun getAllApartmentsWithRoomDetails(): List<ApartmentAndRoomNumberDTO> = dbQuery {
-        val sqlQuery: String = """
-            select a.name as apartment_name, count(r.name) as room_count from flat.apartment a, flat.room r
-            where a.id = r.apartment_id
-            group by a.name
-        """.trimIndent()
-
-        transaction {
-            val result = mutableListOf<ApartmentAndRoomNumberDTO>()
-            exec(sqlQuery) { rs ->
-                while (rs.next()) {
-                    result.add(
-                        ApartmentAndRoomNumberDTO(
-                            rs.getString("apartment_name"),
-                            rs.getString("room_count")
-                        )
-                    )
-                }
+        Apartment
+            .innerJoin(Room)
+            .select(Apartment.name, Room.name.count())
+            .groupBy(Apartment.name)
+            .map { row ->
+                ApartmentAndRoomNumberDTO(
+                    apartmentName = row[Apartment.name],
+                    roomName = row[Room.name.count()].toString(),
+                )
             }
-            result
-        }
+
     }
 
     suspend fun deleteApartment(id: Int): Int = dbQuery {
